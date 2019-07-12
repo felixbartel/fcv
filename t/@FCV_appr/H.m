78d24e92513ae77dd3720129a76853220edc18d5
felixbartel <felixbartel@protonmail.com>
2019-07-12 09:15:05 +0200

    bug fix for the torus

--- t/@FCV_appr/H.m
+++ t/@FCV_appr/H.m
@@ -11,10 +11,10 @@ function [f_r,fhat_r,ddf_r] = H(self,lambda)
   if nargout > 2
     [tmp,~] = lsqr(...
       @(x,transp_flag) Afun(self.plan,x,lambda,self.W,self.What,transp_flag),...
-      [zeros(length(self.W),1);-self.What.*fhat_r],1e-10);
+      [zeros(length(self.W),1);sqrt(self.What/lambda).*fhat_r],1e-10);
     nfft_set_f_hat(self.plan,tmp);
     nfft_trafo(self.plan);
-    ddf_r = nfft_get_f(self.plan);
+    ddf_r = -nfft_get_f(self.plan);
   end
 end
 
